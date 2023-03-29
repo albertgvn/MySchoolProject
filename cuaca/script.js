2bb2c694ff26c25a1e9d66cf75501df2f621208d
albertgvn <67414091+albertgvn@users.noreply.github.com>
2023-03-29 19:03:48 +0700

    Add files via upload

---
+++ cuaca/script.js
@@ -0,0 +1,58 @@
+const api = {
+    key: "da13e445aa2c554543f476b29984ff73",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+  const searchbox = document.querySelector('.search-box');
+  searchbox.addEventListener('keypress', setQuery);
+  
+  function setQuery(evt) {
+    if (evt.keyCode == 13) {
+      getResults(searchbox.value);
+    }
+  }
+  
+  function getResults (query) {
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+    if (xhr.status === 200) {
+        let weather = JSON.parse(xhr.responseText);
+        displayResults(weather);
+    } else {
+        console.error(xhr.statusText);
+    }
+    };
+    xhr.open("GET", `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`);
+    xhr.send();
+  }
+  
+  function displayResults (weather) {
+    let city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+  
+    let now = new Date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+  
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+  
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+  
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
+  
+  function dateBuilder (d) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+  
+    return `${day} ${date} ${month} ${year}`;
+  }
+  
